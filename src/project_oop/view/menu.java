c506701e3ac8b4823150904b672c1ad734bbbbe8
Risky Wahyu <risky.chici@gmail.com>
2025-11-21 21:44:34 +0700

    Membuat tombol sidebar karyawan

--- src/project_oop/view/menu.java
+++ src/project_oop/view/menu.java
@@ -43,6 +43,7 @@ public class menu extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         btnPesanan = new javax.swing.JButton();
         btnDaftarMenu = new javax.swing.JButton();
+        btnKaryawan = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -148,6 +149,13 @@ public class menu extends javax.swing.JFrame {
         btnDaftarMenu.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         getContentPane().add(btnDaftarMenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(11, 279, 266, 63));
 
+        btnKaryawan.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/btn-sidebar-karyawan.png"))); // NOI18N
+        btnKaryawan.setBorderPainted(false);
+        btnKaryawan.setContentAreaFilled(false);
+        btnKaryawan.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnKaryawan.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/image/btn-sidebar-karyawan-hover.png"))); // NOI18N
+        getContentPane().add(btnKaryawan, new org.netbeans.lib.awtextra.AbsoluteConstraints(11, 354, 266, 63));
+
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/main-bg.png"))); // NOI18N
         jLabel3.setToolTipText("");
         getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -206,6 +214,7 @@ public class menu extends javax.swing.JFrame {
     private javax.swing.JButton btnBeranda;
     private javax.swing.JButton btnCari;
     private javax.swing.JButton btnDaftarMenu;
+    private javax.swing.JButton btnKaryawan;
     private javax.swing.JButton btnPesanan;
     private javax.swing.JButton btnTambahMenu;
     private javax.swing.JLabel jLabel1;
